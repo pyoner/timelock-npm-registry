36e28754b0557db5acfb5f0b4dae8f3e2b8c97a0
Askar Yusupov <devex.soft@gmail.com>
2025-09-29 11:51:13 +0500

    build: add npm-registry-sdk dependency to package.json to enable npm package info retrieval feat(index.ts): implement endpoint to fetch npm package info filtered by recent updates based on minutes parameter

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,35 @@
 import { Hono } from "hono";
+import { NpmRegistry, PackageInfo } from "npm-registry-sdk";
+
+// Use default registry URLs
+const registry = new NpmRegistry();
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
-app.get("/message", (c) => {
-  return c.text("Hello Hono!");
+app.get("/:mins{[0-9]+}/:name/:version?", async (c) => {
+  const params = c.req.param();
+
+  const mins = parseInt(params.mins) * 60_000;
+  const date = new Date(Date.now() - mins);
+  console.log(date);
+
+  const pkgInfo = await registry.getPackage(params.name);
+
+  const excludeKeys = ["created", "modified"];
+  for (let k in pkgInfo.time) {
+    if (excludeKeys.includes(k)) {
+      continue;
+    }
+
+    const pkgDate = new Date(pkgInfo.time[k]);
+    if (pkgDate > date) {
+      delete pkgInfo.versions[k];
+      delete pkgInfo.time[k];
+      console.log("removed version:", k, pkgDate.toISOString());
+    }
+  }
+
+  return c.json(pkgInfo);
 });
 
 export default app;
